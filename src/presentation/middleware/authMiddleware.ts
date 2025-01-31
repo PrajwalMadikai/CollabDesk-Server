866e383866abf0111d5e71e29ceceaedd21d8ed6
PrajwalMadikai <133801108+PrajwalMadikai@users.noreply.github.com>
2025-01-31 13:41:50 +0530

    jwt middleware

---
+++ src/presentation/middleware/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenService } from "../../applications/services/TokenService";
+const tokenService = new TokenService();  
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];  
+  if (!token) {
+    res.status(401).json({ message: "Access Denied: No Token Provided" });
+    return;
+  }
+
+  const verified = tokenService.verifyToken(token);
+
+  if ("status" in verified) {
+    res.status(verified.status).json({ message: verified.message });
+    return;
+  }
+
+  req.user = verified;  
+  next();  
+};
--- src/presentation/routes/workspaceRoute.ts
+++ src/presentation/routes/workspaceRoute.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import { WorkspaceUsecase } from '../../applications/usecases/WorkspaceUsecase'
 import { WorkspaceRepository } from '../../respository/WorkspaceRepository'
 import { WorkspaceController } from '../controllers/workspaceController'
+import { authenticateToken } from '../middleware/authMiddleware'
 const router=express.Router()
 
 const workspaceRepository=new WorkspaceRepository()
@@ -12,7 +13,7 @@ const workspaceUsecase=new WorkspaceUsecase(workspaceRepository)
 const workspaceController=new WorkspaceController(workspaceUsecase)
 
 
-router.post('/create',async(req:Request,res:Response,next:NextFunction)=>{
+router.post('/create',authenticateToken,async(req:Request,res:Response,next:NextFunction)=>{
     await workspaceController.newWorkspace(req, res, next);
 })
 
